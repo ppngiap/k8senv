9091cd77996a1c8ac38fe25c81ee2650d9b18979
tom phan <ppngiap@gmail.com>
2021-07-08 11:05:38 -0700

    Adds optional container to kklogs

--- genenv.py
+++ genenv.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import argparse
 
 
 class EnvNode:
@@ -7,28 +8,41 @@ class EnvNode:
         self.var = var
         self.pattern = pattern
         self.abbr = abbr
-        self.pod = ''
+        self.pod = None
+        self.container = None
 
     def match(self, pod):
         return pod.find(self.pattern) != -1
 
+    def __set_container(self, pod):
+        # typical pod has the format of *-59c8c86bcb-b2tr7
+        arr = pod.split('-')
+        try:
+            if len(arr[-1]) == 5 and (len(arr[-2]) == 10 or len(arr[-2]) == 9):
+                self.container = "-".join(arr[0:-2])
+        except IndexError:
+            self.container = None
+
     def set_pod(self, pod):
-        if len(self.pod):
+        if self.pod is not None:
             print("#Pod exist : %s" % self.pod)
             print("#Pod to add: %s" % pod)
         self.pod = pod
+        self.__set_container(self.pod)
 
     def output(self):
-        if len(self.pod) == 0:
+        if self.pod is None:
             print("#export %s=" % self.var)
             return
         print("export %s=%s" % (self.var, self.pod))
         print("alias kkbash%s=\'kubectl exec -it %s -- /bin/bash\'" % (self.abbr, self.pod))
         print("alias kksh%s=\'kubectl exec -it %s -- /bin/sh\'" % (self.abbr, self.pod))
         print("alias kkdp%s=\'kubectl describe pod %s\'" % (self.abbr, self.pod))
-        print("alias kklogs%s=\'kubectl logs %s\'" % (self.abbr, self.pod))
+        if self.container is not None:
+            print("alias kklogs%s=\'kubectl logs %s -c %s\'" % (self.abbr, self.pod, self.container))
+        else:
+            print("alias kklogs%s=\'kubectl logs %s\'" % (self.abbr, self.pod))
         print("alias kkvi%s=\'kkdp%s > /tmp/pod_%s; vi /tmp/pod_%s\'" % (self.abbr, self.abbr, self.pod, self.pod))
-
 
 
 class VmlcEnv:
@@ -81,12 +95,24 @@ def process_pods(text):
     vmlcEnv.output()
 
 
-vmlcEnv.set_template(os.path.expanduser('~/.k8senv/vmlc_template.txt'))
+def main():
+    vmlcEnv.set_template(os.path.expanduser('~/.k8senv/vmlc_template.txt'))
 
-process = subprocess.Popen(['kubectl', 'get', 'pods'],
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE)
-stdout, stderr = process.communicate()
+    parser = argparse.ArgumentParser(description='Generates script to create various pod environment variables')
+    parser.add_argument('--test', type=str, help='text file contain output of "kubectl get pods"',
+                        default=None, required=False)
+    args = parser.parse_args()
 
-process_pods(stdout)
-print(stderr)
+    if args.test is not None:
+        process_pods(open(os.path.expanduser(args.test)).read())
+    else:
+        process = subprocess.Popen(['kubectl', 'get', 'pods'],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        process_pods(stdout)
+        print(stderr)
+
+
+if __name__ == '__main__':
+    main()
